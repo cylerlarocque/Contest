a2a4b5672ad8e19cb977477819d86867e96f084d
Cyler LaRocque <cyler.larocque@uleth.ca>
2026-01-16 20:14:56 -0700

    updating

---
+++ Kattis/haughtycuisine.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    string item;
+
+    cin >> n >> n;
+    cout << n << endl;
+
+    while (n--) {
+        cin >> item;
+        cout << item << endl;
+    }
+
+    return 0;
+}
